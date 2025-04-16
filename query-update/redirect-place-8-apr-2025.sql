47080ec3eaf0107c654ff861479f1675beab5160
fery kurniawan <ferykurniawan@ferykurniwan.local>
2025-04-16 10:50:26 +0700

    update query

---
+++ query-update/redirect-place-8-apr-2025.sql
@@ -0,0 +1,2 @@
+INSERT INTO seo_geo_page_redirection (source_path, redirection_path, is_permanent) VALUES ('/place/putri-farahnia-a-tembalang-semarang', '/place/putra-alfaro-tembalang-semarang', true);
+INSERT INTO seo_geo_page_redirection (source_path, redirection_path, is_permanent) VALUES ('/place/fortaleza-tangerang', '/place/fortaleza-karawaci', true);
